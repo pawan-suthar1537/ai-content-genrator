10165962bd29818fc2ee3046c3a498e8e5867a85
pawansuthar <pawansuthar15372097023627153@gmail.com>
2024-06-27 15:32:31 +0530

    sidenav added

--- app/dashboard/_components/Sidenav.tsx
+++ app/dashboard/_components/Sidenav.tsx
@@ -1,8 +1,32 @@
+import { FileClock, Home, Settings2, WalletIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 const Sidenav = () => {
+  const MENULIST = [
+    {
+      name: "Home",
+      icon: Home,
+      href: "/dashboard",
+    },
+    {
+      name: "History",
+      icon: FileClock,
+      href: "/dashboard/history",
+    },
+    {
+      name: "Billing",
+      icon: WalletIcon,
+      href: "/dashboard/billing",
+    },
+    {
+      name: "Setting",
+      icon: Settings2,
+      href: "/dashboard/setting",
+    },
+  ];
+
   return (
     <div className="h-screen p-5 shadow-sm border">
       <Link href="/">
@@ -16,6 +40,14 @@ const Sidenav = () => {
           />
         </div>
       </Link>
+      <div className="mt-5">
+        {MENULIST.map((item, index) => (
+          <div className="flex gap-2 p-3 mb-3 rounded-lg hover:bg-primary hover:text-white cursor-pointer">
+            <item.icon />
+            <h2>{item.name}</h2>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
