295dc4360206ee55eb2414955a9e19acaede62a9
pawansuthar <pawansuthar15372097023627153@gmail.com>
2024-06-27 18:07:22 +0530

    texteditor added

--- app/dashboard/content/_components/RichTextedior.tsx
+++ app/dashboard/content/_components/RichTextedior.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useRef } from "react";
+import "@toast-ui/editor/dist/toastui-editor.css";
+
+import { Editor } from "@toast-ui/react-editor";
+import { Button } from "@/components/ui/button";
+import { Copy } from "lucide-react";
 
 const RichTextedior = () => {
-  return <div>RichTextedior</div>;
+  const editorref: any = useRef();
+  return (
+    <div className="bg-white shadow-lg border rounded-lg">
+      <div className="flex justify-between items-center p-5">
+        <h2 className="font-medium text-lg">Your Result</h2>
+        <Button>
+          <Copy />
+          Copy
+        </Button>
+      </div>
+      <Editor
+        ref={editorref}
+        initialValue="Your Result will appear here."
+        height="600px"
+        initialEditType="wysiwyg"
+        useCommandShortcut={true}
+        onChange={() =>
+          console.log(editorref.current.getInstance().getMarkdown())
+        }
+      />
+    </div>
+  );
 };
 
 export default RichTextedior;
