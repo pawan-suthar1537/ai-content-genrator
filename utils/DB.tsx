01ed15c7daa7cd8e4deb476d145bd5536c8d268a
pawansuthar <pawansuthar15372097023627153@gmail.com>
2024-06-29 15:25:16 +0530

    esrdthrstdjfhteyrtjhgkjtryethg

--- utils/DB.tsx
+++ utils/DB.tsx
@@ -1,5 +1,12 @@
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./Schema";
-const sql = neon(process.env.NEXT_PUBLIC_DRIZZLE_DB!);
+
+// Ensure the environment variable is set
+const connectionString = process.env.NEXT_PUBLIC_DRIZZLE_DB;
+if (!connectionString) {
+  throw new Error("No database connection string was provided to `neon()`.");
+}
+
+const sql = neon(connectionString);
 export const db = drizzle(sql, { schema });
